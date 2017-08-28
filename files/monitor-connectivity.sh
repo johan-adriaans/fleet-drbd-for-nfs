de6c19244720ca2d47bf033e07ca0961599ab8df
Johan Adriaans <johan@izi-services.nl>
2017-08-28 16:25:38 +0200

    Improved connectivity test

--- files/monitor-connectivity.sh
+++ files/monitor-connectivity.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 # Keep running until etcd connectivity is lost, fail after MAX_FAILURES tries
-ETCD_URL="http://127.0.0.1:2379/v2/keys"
+#ETCD_URL="http://127.0.0.1:2379/v2/keys"
+ETCD_URL="http://10.0.0.10:2379/v2/keys"
 MAX_FAILURES=3
 
 echo "Starting cluster connectivity monitor, maximum failures set to $MAX_FAILURES"
 
 COUNT=0
 while true; do
-  if [ "$(curl --silent --connect-timeout 2 "$ETCD_URL")" ]; then
+  OUTPUT="$(curl --silent --connect-timeout 2 "$ETCD_URL")"
+  if [ $? ] && [[ $OUTPUT == '{"action":"get",'* ]] ; then
     if [ $COUNT -ne 0 ]; then
       echo "Successfull connect after $COUNT failures, resetting counter"
     fi
